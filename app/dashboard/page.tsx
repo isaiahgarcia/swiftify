55fac72828398951acb597350eaa1fb9da17bbc8
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2024-02-06 18:57:25 -0800

    ig - created swiftie breakdown pie chart and populated half of taylor song to album mapping

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,12 +11,14 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { PieChart } from "@/components/Chart";
 import { redirect } from "next/navigation";
+import { albumIdx, songsToAlbums } from "@/lib/TaylorSongs";
 
 export default function DashboardPage() {
   const { data: session, status } = useSession();
 
   const [topNonTaylorSongs, setTopNonTaylorSongs] = useState<Track[]>([]);
   const [topTaylorSongs, setTopTaylorSongs] = useState<Track[]>([]);
+  const [topTaylorSongsByAlbum, setTopTaylorSongsByAlbum] = useState<Track[]>([]);
 
   // Effect to establish a protected route if user is not signed in
   useLayoutEffect(() => {
@@ -29,32 +31,37 @@ export default function DashboardPage() {
     async function displayTopTaylorSongs() {
       if (session && session.accessToken) {
         // Grab current user's top Taylor Swift tracks
-        // const data = await SpotifyApiRequest(`https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term`, session.accessToken);
-        // if (data && data.items) {
-        //   const topTaylorSongs = data.items.filter((track: any) => track.artists.map((artist: any) => artist.id).includes('06HL4z0CvFAxyc27GXpf02'));
-        //   const topNonTaylorSongs = data.items.filter((track: any) => !track.artists.map((artist: any) => artist.id).includes('06HL4z0CvFAxyc27GXpf02'));
-        //   setTopNonTaylorSongs(topNonTaylorSongs);
-        //   setTopTaylorSongs(topTaylorSongs);
-        // }
+        const data = await SpotifyApiRequest(`https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term`, session.accessToken);
+        if (data && data.items) {
+          const topTaylorSongs = data.items.filter((track: any) => track.artists.map((artist: any) => artist.id).includes('06HL4z0CvFAxyc27GXpf02'));
+          const topNonTaylorSongs = data.items.filter((track: any) => !track.artists.map((artist: any) => artist.id).includes('06HL4z0CvFAxyc27GXpf02'));
+          const albumsFromTopSongs = topTaylorSongs.map((track: any) => { return (track.name in songsToAlbums) ? songsToAlbums[track.name] : "" });
+          const topSongBreakdown = new Array(10).fill(0);
+          for (const album of albumsFromTopSongs) {
+            if (album != "") topSongBreakdown[albumIdx[album]] += 1;
+          }
+          setTopTaylorSongsByAlbum(topSongBreakdown);
+          setTopNonTaylorSongs(topNonTaylorSongs);
+          setTopTaylorSongs(topTaylorSongs);
+        }
       }
     }
     displayTopTaylorSongs();
-  }, [session, topTaylorSongs]);
+  }, [session]);
 
   return (
     <>
       <NavMenu />
       <div className="w-3/4 flex flex-col items-center space-y-2">
-        <div className="flex flex-row space-x-20">
+        <div className="flex">
           <Card>
             <CardHeader>
-              <CardTitle className="text-center">Top Song Stats</CardTitle>
+              <CardTitle className="text-center">Your Swiftie Breakdown</CardTitle>
             </CardHeader>
             <CardContent>
-              <PieChart seriesData={[topTaylorSongs.length, topNonTaylorSongs.length]} labelNames={["Taylor songs", "Non-Taylor songs"]} />
+              <PieChart seriesData={topTaylorSongsByAlbum} labelNames={["Taylor Swift", "Fearless", "Speak Now", "Red", "1989", "Reputation", "Lover", "folklore", "evermore", "Midnights"]} />
             </CardContent>
           </Card>
-          <p>Chart</p>
         </div>
         <Carousel className="max-w-xs">
           <CarouselContent>
